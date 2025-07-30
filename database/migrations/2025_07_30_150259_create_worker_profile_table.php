84e3dc45cb11c6e2f44b76d9bc3afe592c21e8f8
= <=>
2025-07-30 20:46:09 +0500

    worker_profile and services table createed

---
+++ database/migrations/2025_07_30_150259_create_worker_profile_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('worker_profile', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('service_id');
+            $table->string('CNIC_number', 20)->unique();
+            $table->integer('experience_years');
+            $table->string('city');
+            $table->enum('admin_approved', [0, 1])->default(0);
+            $table->string('profile_image');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('service_id')->references('id')->on('services');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('worker_profile');
+    }
+};
