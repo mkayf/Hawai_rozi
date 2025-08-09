c9f6f2105a37ebab950c5e1a0e2e9c8557ae75cf
abdu1 samad <sammadaltaf43@gmail.com>
2025-08-08 18:29:11 -0700

    hero section working

---
+++ resources/views/components/heroSection.blade.php
@@ -0,0 +1,21 @@
+
+<section class="hero-section">
+    <div class="hero-section-container">
+        <div class="hero-section-bg-img">
+            <div class="hero-section-content">
+                <div class="hero-section-label">
+                    <label for="">Hire Trusted Professionals</label>
+                </div>
+                <div class="hero-section-heading">
+                    <h3>  hire an expert <span>Hawayi Rozi</span></h3>
+                </div>
+                <div class="hero-section-para">
+                    <p>On Hawayi Rozi, plumbers, electricians, painters, and multiple experts of every skill are always ready for your work</p>
+                </div>
+                <div class="hero-section-btn">
+                    <button>Join As Worker</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
