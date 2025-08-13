685daec2f2b0fd2a075483443b091478a0a483d2
abdu1 samad <sammadaltaf43@gmail.com>
2025-08-12 20:41:22 -0700

    benifits section is ready

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -24,6 +24,10 @@
     <!-- services-section-css-link  -->
     @vite('resources/css/services.css')
 
+    
+    <!-- services-section-css-link  -->
+    @vite('resources/css/benefits.css')
+
     <!-- font-awesome-cdn  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
