aa50012380eb4904c78a2f7e821eff1e48923467
mkayf <muhammadkaif150@gmail.com>
2025-08-21 00:42:22 +0500

    checking

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,11 +8,8 @@
 
     {{-- bootstrap-cdn-link  --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-<<<<<<< HEAD
-=======
     {{-- AOS-Animation-link --}}
     <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
->>>>>>> 8e00b593057412f5d7fdb4fe65d567bf5adbd884
 
     {{-- app.css --}}
     @vite('resources/css/app.css')
@@ -23,7 +20,6 @@
     <!-- footer-css-link  -->
     @vite('resources/css/footer.css')
 
-<<<<<<< HEAD
     <!-- hero-section-css-link  -->
     @vite('resources/css/heroSection.css')
 
@@ -34,11 +30,6 @@
     <!-- services-section-css-link  -->
     @vite('resources/css/benefits.css')
 
-=======
-     <!-- hero-section-css-link  -->
-    @vite('resources/css/heroSection.css')
-
->>>>>>> 8e00b593057412f5d7fdb4fe65d567bf5adbd884
     <!-- font-awesome-cdn  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
@@ -48,16 +39,9 @@
     @include('components.navbar')
 
     @yield('content')
-<<<<<<< HEAD
     
     @include('components.footer')
     
-=======
-
-    @include('components.footer')
-    
-
->>>>>>> 8e00b593057412f5d7fdb4fe65d567bf5adbd884
     {{-- bootstrap-js-link  --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     {{-- app.js --}}
